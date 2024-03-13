7c50344d2530481fb3c4e27b10711ff3e7f8b150
Allyson Reis <allysonreis1@gmail.com>
2024-03-13 17:47:47 -0300

    Add test for empty search results in CategoryRepository

--- BackEnd/CodeFlix.Admin.Catalogo/tests/FC.CodeFlix.Catalog.IntegrationTests/Infra.Data.EF/Repositories/CategoryRepository/CategoryRepositoryTest.cs
+++ BackEnd/CodeFlix.Admin.Catalogo/tests/FC.CodeFlix.Catalog.IntegrationTests/Infra.Data.EF/Repositories/CategoryRepository/CategoryRepositoryTest.cs
@@ -153,4 +153,24 @@ public class CategoryRepositoryTest
         }
 
     }
+
+    [Fact]
+    public async Task SearchReturnsEmptyWhenPersistenceIsEmpty()
+    {
+        CodeFlixCatalogDbContext dbContext = _fixture.CreateDbContext();
+
+        var categoryRepository = new Repository.CategoryRepository(dbContext);
+
+        var searchInput = new SearchInput(1, 20, "", "", SearchOrder.Asc);
+
+        var output = await categoryRepository.Search(searchInput, CancellationToken.None);
+
+        output.Should().NotBeNull();
+        output.Items.Should().NotBeNull();
+        output.CurrentPage.Should().Be(searchInput.Page);
+        output.PerPage.Should().Be(searchInput.PerPage);
+        output.Total.Should().Be(0);
+        output.Items.Should().HaveCount(0);
+
+    }
 }
