1c8381eac850eb29af860346b598597066d5d4db
Allyson Reis <allysonreis1@gmail.com>
2024-03-19 21:36:29 -0300

    Update the Search to return paginated list

--- BackEnd/CodeFlix.Admin.Catalogo/src/FC.CodeFlix.Catalog.Infra.Data.EF/Repositories/CategoryRepository.cs
+++ BackEnd/CodeFlix.Admin.Catalogo/src/FC.CodeFlix.Catalog.Infra.Data.EF/Repositories/CategoryRepository.cs
@@ -37,8 +37,12 @@ public class CategoryRepository : ICategoryRepository
 
     public async Task<SearchOutput<Category>> Search(SearchInput searchInput, CancellationToken cancellationToken)
     {
+        var toSkip = searchInput.PerPage * (searchInput.Page - 1);
         var total = await _categories.CountAsync(cancellationToken);
-        var items = await _categories.ToListAsync(cancellationToken);
+        var items = await _categories.AsNoTracking()
+            .Skip(toSkip)
+            .Take(searchInput.PerPage)
+            .ToListAsync(cancellationToken);
         return new SearchOutput<Category>(searchInput.Page, searchInput.PerPage, total, items);
     }
 
